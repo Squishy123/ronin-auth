533f93f014a0f010774ef263d2bb35c927cf805f
squishy123 <christian.gnaw@gmail.com>
2019-09-10 10:38:16 -0400

    Added changes to docker-compose and schema for user model

---
+++ src/app/models/user.js
@@ -0,0 +1,20 @@
+
+import mongoose from 'mongoose';
+
+//Defined Schema
+const Schema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    is_verified: Boolean,
+    verification_token: String,
+    reset_token: String,
+    transfer_token: String
+});
+
+let User = mongoose.model('User', Schema);
+
+//Model Functions
+User.getAll = () => User.find({});
+
+export default User;
